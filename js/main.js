5c1e38f7eba922740404086731300acd185c5847
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-06-23 16:25:02 -0300

    Add & apply styles to back top button

--- js/main.js
+++ js/main.js
@@ -24,12 +24,15 @@ addEventsOnElements(navTogglers, "click", toggleNav);
 // Header animation: when scrolled down to 100px header will be active
 
 const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
 
 window.addEventListener("scroll", () => {
     if(window.scrollY > 100){
         header.classList.add("active");
+        backTopBtn.classList.add("active");
     } else {
         header.classList.remove("active");
+        backTopBtn.classList.remove("active");
     }
 });
 
