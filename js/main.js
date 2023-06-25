dda6895cb193bf4cbc4e137e0ddf41d8e3b45c43
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-06-25 14:01:37 -0300

    Add mobile navbar toggler & header animation when scrolled down to 100px

--- js/main.js
+++ js/main.js
@@ -1,29 +1,22 @@
-'use strict';
-
-// Add event listener on multiple events
-
-const addEventsOnElements = function(elements, eventType, callback){
-    for(let i=0; i < elements.length; i++){
-        elements[i].addEventListener(eventType,callback);
-    }
-} 
-
 // Mobile navbar toggler
 
-const navbar = document.querySelector("[data-navbar]");
-const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navbar = document.querySelector("#navbar");
+const navTogglers = document.querySelectorAll(".nav-toggler");
 
 const toggleNav = () => {
     navbar.classList.toggle("active");
-    // document.body.classList.toggle("nav-active");
 } 
 
-addEventsOnElements(navTogglers, "click", toggleNav);
+navTogglers.forEach(navToggler => {
+    navToggler.addEventListener('click', () => {
+        toggleNav();
+    });
+})
 
 // Header animation: when scrolled down to 100px header will be active
 
-const header = document.querySelector("[data-header]");
-const backTopBtn = document.querySelector("[data-back-top-btn]");
+const header = document.getElementById("header");
+const backTopBtn = document.getElementById("back-top");
 
 window.addEventListener("scroll", () => {
     if(window.scrollY > 100){
@@ -87,19 +80,4 @@ window.addEventListener("resize", function (){
     
     moveSliderItem();
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
